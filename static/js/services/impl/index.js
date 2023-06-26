ec98232238140294222f3202900c9e24276af671
JbyteSecurity <jordandiaz2016@gmail.com>
2023-06-26 10:31:00 -0500

    Se agrego encriptacion

--- static/js/services/impl/index.js
+++ static/js/services/impl/index.js
@@ -38,3 +38,26 @@ export const generateChargeImpl = async ({tokenId,  email, parameters3DS = null}
 
   return service.createCharge(parameters3DS ? { ...data, authentication_3DS: { ...parameters3DS } } : data);
 }
+
+
+export const generateChargeEncryptImpl = async ({
+                                           email,
+                                           tokenId,
+                                           deviceId,
+                                           parameters3DS = null,
+                                         }) => {
+  const bodyRequest = {
+    amount: config.TOTAL_AMOUNT,
+    currency_code: config.CURRENCY,
+    email: email,
+    source_id: tokenId,
+    antifraud_details: {
+      device_finger_print_id: deviceId,
+    },
+  };
+  return service.generateChargeEncrypt(
+      parameters3DS
+          ? { ...bodyRequest, authentication_3DS: { ...parameters3DS } }
+          : bodyRequest
+  );
+};
